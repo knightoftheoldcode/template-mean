9a199f33aec6e7c889bac21f2292eab32e8369e6
Curtis Spendlove <curtis@knightoftheoldcode.com>
2017-08-28 23:25:57 -0600

    Refactor into Server Directory
    I like to organize my code better than the default directory structure.
    This is a persnickety habit, to be sure. That said, I like to be able to
    find my code in a structure that feels logical to me.
    
    - create `server` directory
    - create `server/models` and `server/controllers`
    - move `routes` and `views` to `server`
    - modify code to reference new file locations

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var lessMiddleware = require('less-middleware');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./server/routes/index');
+var users = require('./server/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
